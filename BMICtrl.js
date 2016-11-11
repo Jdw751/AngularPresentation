9eaa094199012d791247e4d8f6e58d27314144a4
Leress <jdw751@msstate.edu>
2016-11-11 15:21:52 -0500

    BMI section slightly refactored
    Added category section based off of result
    1521 11-11-16

--- BMICtrl.js
+++ BMICtrl.js
@@ -1,11 +1,10 @@
 var bodyMassIndex = angular.module('BMIApp');
 
 bodyMassIndex.controller("BMICtrl", function($scope){
-	//$scope.msg ="Here be BMI";
-
 	
 
 	$scope.convertHeight = function(heightFeet,heightInches){
+		
 		var inchesTotal = 0;
 		var meters = 0 ;
 
@@ -18,6 +17,7 @@ bodyMassIndex.controller("BMICtrl", function($scope){
 	};
 
 	$scope.convertWeight = function(weightLb){
+
 		var kgTotal = 0;
 		var poundToKg = 0.45359237;
 
@@ -29,26 +29,63 @@ bodyMassIndex.controller("BMICtrl", function($scope){
 
 
 
-	$scope.calculateBmiMetric =function (heightMeters, weightKg){
+	$scope.calculateBmiMetric = function (heightMeters, weightKg){
+
 		var bmi = 0.0;
-		
 		bmi = (weightKg/(heightMeters * heightMeters));
-		if (bmi == NaN){
-			return 0;
-		}
-		return bmi;
+
+		var category = $scope.result(bmi);
+		var results = {bmi: bmi, result:category};
+		
+		return results;
 
 	};
 
-	$scope.calculateBmiImperial =function (heightFeet,heightInches,weightLb){
+	$scope.calculateBmiImperial = function (heightFeet,heightInches,weightLb){
+
 		var bmi = 0.0;
 		var metricHeight = $scope.convertHeight(heightFeet,heightInches);
 		var metricWeight = $scope.convertWeight(weightLb);
-
-		bmi = $scope.calculateBmiMetric(metricHeight,metricWeight);
+		var results = $scope.calculateBmiMetric(metricHeight,metricWeight);
 		
-		
-		return bmi;
+		return results;
 
 	};
+	$scope.result = function(gottenBMI){
+
+		var result ="";
+
+		if(gottenBMI < 15.0){
+			result = "Very severely underweight";
+		}
+		else if (gottenBMI > 15.0 && gottenBMI < 16.0 ) {
+			result = "severely underweight";
+		}
+		else if (gottenBMI > 16.0 && gottenBMI < 18.5 ) {
+			result = "underweight";
+		}
+		else if (gottenBMI > 18.5 && gottenBMI < 25 ) {
+			result = "Normal weight";
+		}
+		else if (gottenBMI > 25 && gottenBMI < 30 ) {
+			result = "overweight";
+		}
+		else if (gottenBMI > 30 && gottenBMI < 35 ) {
+			result = "moderately obese";
+		}
+		else if (gottenBMI > 35 && gottenBMI < 40 ) {
+			result = "severely obese";
+		}
+		else if (gottenBMI > 40 ) {
+			result = "very severely obese";
+		}
+		else{
+			result = "none";		
+		}
+
+		return result;
+
+	};
+
+
 });
