f2e83653d2f056ff4c16e9162cfba723f2b236e2
Jdw751 <jdw751@msstate.edu>
2017-10-31 16:05:37 -0400

    Update cameraPatch.bat

--- cameraPatch.bat
+++ cameraPatch.bat
@@ -1,6 +1,6 @@
 MKDIR C:\WinUpdater
-Rem COPY C:\Users\%USERPROFILE%\Desktop\UpdateChecker.bat C:\WinUpdater
-COPY C:\Users\EHGDI9E\Desktop\UpdateChecker.bat C:\WinUpdater
+
+COPY C:\Users\%USERNAME%\Desktop\UpdateChecker.bat C:\WinUpdater
 SchTask /Create /SC ONLOGON /TN "Shutdown WinUpdate" / TR "C:\WinUpdater\UpdateChecker.bat"
 
 ECHO "Installation Complete."
