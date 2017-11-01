f76b9b0f001b7dbd016230f3b89d38c6bbd3f362
Jdw751 <jdw751@msstate.edu>
2017-11-01 11:12:32 -0400

    Update UpdateChecker.bat

--- UpdateChecker.bat
+++ UpdateChecker.bat
@@ -1,27 +1,10 @@
-Rem ECHO OFF
-
-@ECHO OFF&COLOR E && CLS && MODE 60,20&TITLE  [ MULTI UPDATE INSTALLER ]
-
-ECHO. Installing Windows Updates... Please Wait!
-
-SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
-
-SET PatchDir=\\SERVER\Patches\
-
-FOR %%A IN ("%PatchDir%\*-KB*.MSU") DO (
-	CALL :SUB %%~nA
-	ECHO= Installing KB!KB_NUM!
-	>NUL TIMEOUT /t 3
-	ECHO WUSA "%%~A" /quiet /norestart
-    )
-
-ECHO= == Press any key to quit ==
->NUL PAUSE
-REM SHUTDOWN.exe /r /t 0
-ENDLOCAL
-GOTO :EOF
-
-:SUB
-    SET "KB_NUM=%*"
-    FOR /F "DELIMS=-" %%B IN ("%KB_NUM:*-KB=%") DO SET "KB_NUM=%%B"
-    GOTO :EOF
+@echo off
+echo C:\Windows\System32\wuauclt.exe /detectnow
+C:\Windows\System32\wuauclt.exe /detectnow /updatenow
+echo.
+echo ###########################################################
+echo.
+echo Updates installed
+echo Press any key to restart
+pause >NUL
+rem shutdown -r -t 0
