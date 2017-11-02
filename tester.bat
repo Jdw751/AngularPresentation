01f83a4385f500348f97a2c682b498652ce9e6d9
Jdw751 <jdw751@msstate.edu>
2017-11-02 15:48:25 -0400

    Update tester.bat

--- tester.bat
+++ tester.bat
@@ -1,10 +1,6 @@
-@echo off
-echo C:\Windows\System32\wuauclt.exe /detectnow
-C:\Windows\System32\wuauclt.exe /detectnow /updatenow
-echo.
-echo ###########################################################
-echo.
-echo Updates installed
-echo Press any key to restart
-pause >NUL
-rem shutdown -r -t 0
+
+C:\Windows\System32\wuauclt.exe  
+pause
+
+C:\Windows\System32\wuauclt.exe  /detectnow /showwuautoscan
+pause
