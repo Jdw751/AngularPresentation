0f1a6f1bea024b3ef1e78b2d38198d7b1b3c5fe6
Jdw751 <jdw751@msstate.edu>
2017-11-03 09:43:39 -0400

    Update UpdateChecker.bat

--- UpdateChecker.bat
+++ UpdateChecker.bat
@@ -1,10 +1,9 @@
 @echo off
-echo C:\Windows\System32\wuauclt.exe /detectnow
+
 C:\Windows\System32\wuauclt.exe /resetauthorization /detectnow /updatenow
 echo.
 echo ###########################################################
 echo.
 echo Updates installed
-echo Press any key to restart
+
 pause >NUL
-rem shutdown -r -t 0
