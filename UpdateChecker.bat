ed1f7e3c1c7c9d9b5418ea9c32517a5ebcc89c47
Jdw751 <jdw751@msstate.edu>
2017-11-03 11:35:19 -0400

    Update UpdateChecker.bat

--- UpdateChecker.bat
+++ UpdateChecker.bat
@@ -2,7 +2,7 @@
 rem runs a check for updates and when important ones are found, update. 
 rem remove showwuautoscan and "press to close" and pause>NUL once confirmed working.
 rem C:\Windows\System32\wuauclt.exe /a /ResetAuthorization /detectnow /showwuautoscan /updatenow
- C:\Windows\System32\usoclient.exe StartScan
+C:\Windows\System32\usoclient.exe StartScan
 echo.
 echo ###########################################################
 echo.
