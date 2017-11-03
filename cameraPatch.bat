4fdc86af0b53ee688b5972d8e36a238e4cd8355e
Jdw751 <jdw751@msstate.edu>
2017-11-03 09:50:09 -0400

    Update cameraPatch.bat

--- cameraPatch.bat
+++ cameraPatch.bat
@@ -2,8 +2,17 @@ ECHO OFF
 
 MKDIR C:\WinUpdater
 
-COPY C:\Users\%USERNAME%\Desktop\UpdateChecker.bat C:\WinUpdater
-SchTasks /Create /SC ONLOGON /TN "Log On WinUpdate" /TR "C:\WinUpdater\UpdateChecker.bat"
+rem COPY C:\Users\%USERNAME%\Desktop\UpdateChecker.bat C:\WinUpdater
+if exist C:\WinUpdater\UpdateChecker.bat (
+	rem delete old file and replace with newest one.
+	del C:\WinUpdater\UpdateChecker.bat
+	COPY C:\Users\%USERNAME%\Desktop\UpdateChecker.bat C:\WinUpdater
+	
+) else ( 
+	COPY C:\Users\%USERNAME%\Desktop\UpdateChecker.bat C:\WinUpdater )
 
+rem create the scheduled task to run update on log in of device.
+SchTasks /Create /SC ONLOGON /TN "Shutdown WinUpdate" /TR "C:\WinUpdater\UpdateChecker.bat"	
+	
 ECHO "Installation Complete."
 pause
