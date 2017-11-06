f5a113f686c5fbab10e4373f99346b71d8a4f9c5
Jdw751 <jdw751@msstate.edu>
2017-11-06 14:57:54 -0500

    Update tester.bat

--- tester.bat
+++ tester.bat
@@ -4,3 +4,17 @@ pause
 
 C:\Windows\System32\wuauclt.exe  /detectnow /showwuautoscan
 pause
+
+
+
+rem C:\Windows\System32\wuauclt.exe  
+rem C:\Windows\System32\systeminfo.exe
+set theOne = C:\Windows\System32\systeminfo.exe | findstr KB4041676
+pause
+if exist %theOne%(
+	echo Found it 
+	echo %theOne% )
+pause
+
+rem C:\Windows\System32\wuauclt.exe  /detectnow /showwuautoscan /updatenow
+rem pause
