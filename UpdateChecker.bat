8b0889805e93a8acb7e8d3bf2f7f92fb95cd2594
Jdw751 <jdw751@msstate.edu>
2017-11-08 11:32:54 -0500

    Update UpdateChecker.bat

--- UpdateChecker.bat
+++ UpdateChecker.bat
@@ -1,9 +1,15 @@
 @echo off
 rem create restore point before updates.
-cmd.exe /k "Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%DATE%", 100, 7"
+rem start /b cmd.exe /k "Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%DATE%", 100, 7"
+
+rem if update success, runs a check for updates. 
+
+if %ERRORLEVEL% EQU 0 (
+	%windir%\explorer.exe ms-settings:windowsupdate-action
+) 
 
 rem runs a check for updates. 
-%windir%\explorer.exe ms-settings:windowsupdate-action
+rem %windir%\explorer.exe ms-settings:windowsupdate-action
 
 echo.
 echo ###########################################################
